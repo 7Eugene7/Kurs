98db9b217bb4b7c61932b0e3600fa567be4727b0
Eugene Chumakov <zhenyachumakov@gmail.com>
2022-10-25 14:03:15 +0300

    made a header and  a timer and a statistiс

---
+++ js/skript.js
@@ -0,0 +1,46 @@
+$(document).ready((function(){$(".header__burger").click((function(e){$(".header__burger,.header__menu,.header__btn").toggleClass("active"),$("body").toggleClass("lock"),$(".header,.header__menu").toggleClass("color")}))})),$((function(){$(window).on("scroll",(function(){$(window).scrollTop()>50?$(".header").addClass("active"):$(".header").removeClass("active")}))}));
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+   
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+   
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+   
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+   
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+   
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+   
+  var deadline = new Date(Date.parse(new Date('November 01 2022')) + 11 * 19 * 60 * 60 * 1000); // for endless timer
+  initializeClock('countdown', deadline);
